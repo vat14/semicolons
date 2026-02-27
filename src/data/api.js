3dd5a79aa1825cf622c3a724857a12c1bc3a9112
vat14 <vatsalsoni77@gmail.com>
2026-02-28 03:50:48 +0530

    first host

--- src/data/api.js
+++ src/data/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8000';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000';
 
 export async function fetchKPIs() {
   const res = await fetch(`${API_BASE}/api/kpis`);
